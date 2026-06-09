3601d82e64a573c8586b7f912fc95f029294c516
Patryk-jpg <114743542+Patryk-jpg@users.noreply.github.com>
2026-06-09 06:10:16 +0200

    US-4 annotations, models

--- ClinicManager.Core/Models/ClinicalNote.cs
+++ ClinicManager.Core/Models/ClinicalNote.cs
@@ -1,11 +1,23 @@
-﻿namespace ClinicManager.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManager.Core.Models;
 
 public class ClinicalNote
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Powiazana wizyta jest wymagana.")]
     public int VisitId { get; set; }
+
+    [Required(ErrorMessage = "Autor notatki jest wymagany.")]
+    [StringLength(450, ErrorMessage = "Identyfikator autora moze miec maksymalnie 450 znakow.")]
     public string AuthorId { get; set; } = string.Empty; // Identity userId
+
+    [Required(ErrorMessage = "Tresc notatki jest wymagana.")]
+    [StringLength(4000, ErrorMessage = "Tresc notatki moze miec maksymalnie 4000 znakow.")]
     public string Content { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Data wpisu notatki jest wymagana.")]
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 
     public Visit Visit { get; set; } = null!;
--- ClinicManager.Core/Models/ProcedurePerformed.cs
+++ ClinicManager.Core/Models/ProcedurePerformed.cs
@@ -1,10 +1,19 @@
-﻿namespace ClinicManager.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManager.Core.Models;
 
 public class ProcedurePerformed
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Powiazana wizyta jest wymagana.")]
     public int VisitId { get; set; }
+
+    [Required(ErrorMessage = "Opis procedury jest wymagany.")]
+    [StringLength(500, ErrorMessage = "Opis procedury moze miec maksymalnie 500 znakow.")]
     public string Description { get; set; } = string.Empty;
+
+    [Range(0d, 1_000_000d, ErrorMessage = "Koszt procedury musi byc liczba nieujemna.")]
     public decimal ServiceCost { get; set; }
 
     public Visit Visit { get; set; } = null!;
